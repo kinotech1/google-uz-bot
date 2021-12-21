20cf8f8d8dd0e136d27534a7573a180c2b349e07
kinotech1 <96439913+kinotech1@users.noreply.github.com>
2021-12-21 13:03:35 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from googletrans import Translator
 updater = Updater('5017426403:AAHF2NQ3aTMDvtTFg-WjI9Nkt-VwslmeL44',use_context = True )
 
 def start(updater,context):
- updater.message.reply_text('🖑Salom,Botimizga xush kelibsiz!Botdan foydalanish uchun @foydali_link_1 kanaliga a'zo bo'lishingiz kerak! ')
+ updater.message.reply_text('🖑Salom,Botimizga xush kelibsiz! ')
  
 def echo(updater,context):
  usr_msg =updater.message.text
